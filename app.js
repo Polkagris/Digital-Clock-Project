09d21f60082435b2dced6d367103a201a21b5f92
Arny <arne.n.bjerkaas@gmail.com>
2018-07-02 23:00:34 +0200

    functional clock logic and basic styling

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-ECHO is on.
+
+function clock(){
+  var theTime = new Date(); //get the current time and date
+  var hours = theTime.getHours(); //get hours from date
+  var minutes = theTime.getMinutes(); //get minutes from date
+  var seconds = theTime.getSeconds(); //get seconds from date
+
+   document.getElementById("hour").innerHTML = hours + ":";
+   document.getElementById("minute").innerHTML = minutes + ":";
+   document.getElementById("second").innerHTML = seconds;
+
+   if(seconds < 9){
+     document.getElementById("second").innerHTML = "0" + seconds;
+   }
+   if(minutes < 9){
+     document.getElementById("minute").innerHTML = "0" + minutes;
+   }
+   if(hours < 9){
+     document.getElementById("hour").innerHTML = "0" + hours;
+   }
+}
+setInterval(clock, 500);
